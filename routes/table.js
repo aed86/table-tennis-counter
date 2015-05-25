08e04788f89ebe96f511518707424060feb0ac85
Eugeny Akhmetgareev <akhmetgareev@gmail.com>
2015-05-25 17:06:09 +0300

    Вывод данных в таблице

--- routes/table.js
+++ routes/table.js
@@ -12,28 +12,15 @@ router.get('/', function (req, res) {
  * Страница таблицы
  */
 router.get('/table', function (req, res, next) {
-    res.render('table', {
-        data: [
-            {
-                "id": 923456,
-                "player1": "Brant Lloyd",
-                "player2": "Knox Ambrose",
-                "score": "0:0"
-
-            },
-            {
-                "id": 923457,
-                "player1": "Rosy Stacie",
-                "player2": "Rebecca Tracey",
-                "score": "0:0"
-            },
-            {
-                "id": 923458,
-                "player1": "Marion Arlo",
-                "player2": "Garland Marty",
-                "score": "0:0"
-            }
-        ]
+    Tables.find().sort({'created': '-1'}).exec(function(err, data) {
+        if (err) {
+            res.statusCode(200);
+            log.debug('Error get carrier');
+        } else {
+            res.render('table', {
+                data: data
+            });
+        }
     });
 });
 
