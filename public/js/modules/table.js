0c119e2d792d18a332db9124d173e70a2941a75a
Eugeny Akhmetgareev <akhmetgareev@gmail.com>
2015-05-26 09:37:36 +0300

    Исправлено добавление новой игры

--- public/js/modules/table.js
+++ public/js/modules/table.js
@@ -21,23 +21,24 @@ var Table = {
                     "player2": player2Value
                 },
                 success: function(response) {
-                    //TODO: добавить сюда добавление строки
+                    var players = "" +
+                        "<tr>" +
+                        "<td>" + player1Value + "</td>" +
+                        "<td>0 : 0</td>" +
+                        "<td>" + player2Value + "</td>" +
+                        "<td>" +
+                        "<button data-id='" + response.id + "' class='play'>Play</button>" +
+                        "</td>" +
+                        "</tr>";
+
+                    $('.players table').prepend(players);
+                    $player1.val('').focus();
+                    $player2.val('');
+                },
+                error: function() {
+                    // TODO: show error
                 }
             });
-
-            var players = "" +
-                "<tr>" +
-                    "<td>" + player1Value + "</td>" +
-                    "<td>0 : 0</td>" +
-                    "<td>" + player2Value + "</td>" +
-                    "<td>" +
-                        "<button data-id='' class='play'>Play</button>" +
-                    "</td>" +
-                "</tr>";
-
-            $('.players table').prepend(players);
-            $player1.val('').focus();
-            $player2.val('');
         });
     }
 }
